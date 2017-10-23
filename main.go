48962211aac87ebe409e008c3adfb5f035af1034
Halvor Bakken Smedås <stektpotet@gmail.com>
2017-10-23 19:28:27 +0200

    simple httpserve

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"net/http"
+)
 
+const baseURL = "http://api.fixer.io/latest?base=EUR"
+
+//ServiceHandler - TODO
+func ServiceHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("content-type", "application/json")
+	w.Write([]byte(`{"test":"wat"}`))
+}
+
+func main() {
+	http.HandleFunc("/", ServiceHandler)
 }
