85a3d1f1dde9e10db4618996f6c51fb068f0662c
Halvor Bakken Smedås <stektpotet@gmail.com>
2017-10-23 20:06:18 +0200

    minor main-change

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -8,17 +9,27 @@ import (
 
 const baseURL = "http://api.fixer.io/latest?base=EUR"
 
+//Something - somethis
+type Something struct {
+	Key   uint16 `json:"key"`
+	Value string `json:"value"`
+}
+
 //ServiceHandler - TODO
 func ServiceHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("content-type", "application/json")
-	w.Write([]byte(`{"test":"wat"}`))
+	s := Something{}
+	s.Key = 0
+	s.Value = "HOOOOH BOI"
+	json.NewEncoder(w).Encode(s)
 }
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		log.Fatal("$PORT must be set")
+		log.Fatal("$PORT must be set. Defaulting to 8085")
+		port = "8085"
 	}
-	http.HandleFunc("/", ServiceHandler)
+	http.HandleFunc("/test/", ServiceHandler)
 	http.ListenAndServe(":"+port, nil)
 }
